f1f3ca35029856f7355aad8443b217920118e41e
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-09 18:15:32 +0500

    print used

--- 002a_string_methods.rb
+++ 002a_string_methods.rb
@@ -5,3 +5,4 @@ my_badly_formatted_string = "                Hello, how are you?           "
 
 # change the string with built in methods
 print "My name is " + my_name + " and my website is " + my_website + ".\n"
+print "My name is " + my_name.upcase + " and my website is " + my_website.capitalize 
