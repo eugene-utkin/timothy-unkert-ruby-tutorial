6ddea0e270c86f596596a0f375934d8a61965469
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-09 18:29:19 +0500

    capitalize example

--- 002a_string_methods.rb
+++ 002a_string_methods.rb
@@ -7,3 +7,4 @@ my_badly_formatted_string = "                Hello, how are you?           "
 print "My name is " + my_name + " and my website is " + my_website + ".\n"
 print "My name is " + my_name.upcase + " and my website is " + my_website.capitalize + ".\n"
 print "My name is " + my_name.downcase + " and my website is " + my_website + ".\n"
+puts "book title that's really cool".capitalize
