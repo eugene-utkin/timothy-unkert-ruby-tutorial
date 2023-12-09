cf3b599ac183327541824bcf3eb2a4a37643fffb
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-09 18:18:08 +0500

    downcase example

--- 002a_string_methods.rb
+++ 002a_string_methods.rb
@@ -6,3 +6,4 @@ my_badly_formatted_string = "                Hello, how are you?           "
 # change the string with built in methods
 print "My name is " + my_name + " and my website is " + my_website + ".\n"
 print "My name is " + my_name.upcase + " and my website is " + my_website.capitalize + ".\n"
+print "My name is " + my_name.downcase + " and my website is " + my_website + ".\n"
