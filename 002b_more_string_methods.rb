5bf50745ded7a434a37dcca502b2749e26d19c90
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-10 17:37:17 +0500

    puts used

--- 002b_more_string_methods.rb
+++ 002b_more_string_methods.rb
@@ -7,3 +7,4 @@ puts my_name[0] # print out 'E', the first character of the string
 puts my_name[-1] # print out 'n', the last character of the string
 
 # remember, indices start at 0 in most programming languages (except R and a few others)
+puts my_name[2]
