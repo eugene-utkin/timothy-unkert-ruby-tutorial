49d7399f054c59de4053aa8112c766632797779e
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-10 16:57:38 +0500

    previous comment modified

--- 002b_more_string_methods.rb
+++ 002b_more_string_methods.rb
@@ -3,5 +3,5 @@ my_name = "Evgeniy Utkin"
 my_website = "https://unkertmedia.com"
 
 # printing out specific characters in a string
-puts my_name[0] # print out 'E'
+puts my_name[0] # print out 'E', the first character of the string
 puts my_name[-1] # print out 'n', the last character of the string
