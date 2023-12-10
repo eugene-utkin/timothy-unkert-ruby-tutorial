ff8649e9f570fa175fe634abe575b0947af9f38c
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-10 16:59:02 +0500

    comment added

--- 002b_more_string_methods.rb
+++ 002b_more_string_methods.rb
@@ -5,3 +5,5 @@ my_website = "https://unkertmedia.com"
 # printing out specific characters in a string
 puts my_name[0] # print out 'E', the first character of the string
 puts my_name[-1] # print out 'n', the last character of the string
+
+# remember, indices start at 0 in most programming languages (except R and a few others)
