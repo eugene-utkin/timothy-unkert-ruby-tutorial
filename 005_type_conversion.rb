dae2f2cab429ea09a2b1b0529d408d01a29aad70
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-14 18:10:27 +0500

    problematic puts commented out

--- 005_type_conversion.rb
+++ 005_type_conversion.rb
@@ -1,5 +1,5 @@
 my_name = "Evgeniy"
 my_age = 37
 
-puts "My name is " + my_name + " and I am " + my_age + " years old."
+# puts "My name is " + my_name + " and I am " + my_age + " years old."
 # TypeError - no implicit conversion of Integer into String
