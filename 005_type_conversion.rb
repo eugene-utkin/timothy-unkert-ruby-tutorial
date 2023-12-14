d7ff09a6a94660fc83fafb40f10324d358e46df0
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-14 18:11:43 +0500

    to_s example

--- 005_type_conversion.rb
+++ 005_type_conversion.rb
@@ -3,3 +3,5 @@ my_age = 37
 
 # puts "My name is " + my_name + " and I am " + my_age + " years old."
 # TypeError - no implicit conversion of Integer into String
+
+puts "My name is " + my_name + " and I am " + my_age.to_s + " years old."
