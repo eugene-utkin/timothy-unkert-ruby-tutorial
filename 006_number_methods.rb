72653959b62e4dc89c4560acb6507a0cc1ad1c84
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-15 17:54:12 +0500

    comment added

--- 006_number_methods.rb
+++ 006_number_methods.rb
@@ -9,4 +9,4 @@ dec_1, dec_2, dec_3 = 3.14, 2.7845678, 1.2345
 
 # we can use the round method to round numbers to a certain decimal place
 puts dec_1.round(1) # rounding to 1 decimal place
-puts dec_2.round(2)
+puts dec_2.round(2) # round to 2 decimal places
