02f5322162a1db863a7a71281a94dfc02d29758e
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-16 16:20:46 +0500

    comment modified

--- 007_math_class.rb
+++ 007_math_class.rb
@@ -6,4 +6,4 @@ puts Math.sin(BigMath.PI(10) / 4)
 
 puts Math.sqrt(16) # print 4.0 (float)
 
-puts Math.log(64, 8) # prints 2.0, because 8^2 = 64
+puts Math.log(64, 8) # prints 2.0, because 8^2 = 64 (float)
