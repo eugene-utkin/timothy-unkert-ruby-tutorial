1b96d38af96104dbf1e6c680b66beac6cef8eed3
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-17 16:24:03 +0500

    puts used

--- 010_format_specifiers.rb
+++ 010_format_specifiers.rb
@@ -11,3 +11,5 @@ my_age = 37
 puts "My name is %s and my age is %d" % [my_name, my_age]
 
 puts "The value of PI to 5 decimal places is %.5f" % [BigMath.PI(10)]
+
+puts "My name is #{my_name} and I am #{my_age} years old."
