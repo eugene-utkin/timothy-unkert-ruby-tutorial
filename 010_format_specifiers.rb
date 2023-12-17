81f39cdbf1c33498bfa4d16b03203b0408dc8bef
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-17 15:46:20 +0500

    format specifiers example

--- 010_format_specifiers.rb
+++ 010_format_specifiers.rb
@@ -4,4 +4,4 @@ include BigMath
 my_name = "Evgeniy"
 my_occupation = "Coder"
 
-print "My name is %s and my occupation is %s.\n"
+print "My name is %s and my occupation is %s.\n" % [my_name, my_occupation]
