f5f0aee67672bb5e4b5b3a4507988d9e40707a0e
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-17 16:36:53 +0500

    separator added

--- 011a_arrays.rb
+++ 011a_arrays.rb
@@ -7,3 +7,4 @@ puts names_list.class # outputs 'Array'
 puts names_list[0] # print "Tim"
 puts names_list[-1] # print "Moses", the last item in the array
 puts names_list[1, 4] # prints out indices 1 through 4 each on a separate line
+puts "============"
