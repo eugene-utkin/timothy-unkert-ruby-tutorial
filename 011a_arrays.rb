68524a54603de135dd1ccd0c201ed79c3b98a2e9
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-17 16:28:34 +0500

    comment added

--- 011a_arrays.rb
+++ 011a_arrays.rb
@@ -1,4 +1,4 @@
 # create a list of names (array of names)
 names_list = Array["Tim", "Sean", "Sarah", "Joe", "Aaron", "Matt", "Moses"]
 
-puts names_list.class
+puts names_list.class # outputs 'Array'
