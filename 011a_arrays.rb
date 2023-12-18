d21433fb5d2dac2c403d1d2f85f21c8218c51f92
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-18 21:08:26 +0500

    puts used

--- 011a_arrays.rb
+++ 011a_arrays.rb
@@ -10,3 +10,4 @@ puts names_list[1, 4] # prints out indices 1 through 4 each on a separate line
 puts "============"
 puts names_list[0, 2] # a little funky thing with Ruby printed indices 0 and 1 but not 2
 puts "============"
+puts names_list[0, 4]
