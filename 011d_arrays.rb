5295291c25a42b33d855ce71750c464c47a992a5
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-19 19:54:27 +0500

    one more sort example

--- 011d_arrays.rb
+++ 011d_arrays.rb
@@ -10,3 +10,5 @@ other_arr = ["A", 23, "B", 7, "a", "!"]
 # puts "#{other_arr.sort}" # ArgumentError - comparison of Integer and String failed
 
 third_arr = ["A", "a", "B", "b", "c", "C"]
+
+puts "#{third_arr.sort}"
