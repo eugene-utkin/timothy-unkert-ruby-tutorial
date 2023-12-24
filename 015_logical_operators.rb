4bf8015d585119d1b66eb2258f5b24e197facad3
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-24 18:08:59 +0500

    one more and example

--- 015_logical_operators.rb
+++ 015_logical_operators.rb
@@ -7,6 +7,6 @@ if is_programmer and is_math_teacher
   puts "You are a programmer and a math teacher!"
 elsif is_math_teacher and is_jobless
   puts "You are a math teacher, but you have no job."
-elsif
-  
+elsif is_programmer and is_jobless
+
 end
