89ec16813186b2a2d1fcc064685e30607019ea9b
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-24 18:07:33 +0500

    puts added

--- 015_logical_operators.rb
+++ 015_logical_operators.rb
@@ -6,5 +6,5 @@ is_jobless = false
 if is_programmer and is_math_teacher
   puts "You are a programmer and a math teacher!"
 elsif is_math_teacher and is_jobless
-
+  puts "You are a math teacher, but you have no job."
 end
