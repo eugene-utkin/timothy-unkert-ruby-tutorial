da199bc721b89df7dff7901faf5226e087ffbe21
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-25 20:46:08 +0500

    each block example

--- 016_star_args.rb
+++ 016_star_args.rb
@@ -1,4 +1,7 @@
 # create a method that finds the minimum value passed into it
 def find_min(*args) # allows me to pass in any amount of arguments
   min_num = args[0]
+  args.each do |number|
+
+  end
 end
