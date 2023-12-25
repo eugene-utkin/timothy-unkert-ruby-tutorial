401014946ff9574210e4679874340206b9317148
Evgeniy Utkin <eugene.utkin@gmail.com>
2023-12-25 19:10:45 +0500

    ! used

--- 015_logical_operators.rb
+++ 015_logical_operators.rb
@@ -3,7 +3,7 @@ is_programmer = true
 is_math_teacher = true
 is_jobless = false
 
-if is_programmer and is_math_teacher
+if !is_programmer and is_math_teacher
   puts "You are a programmer and a math teacher!"
 elsif is_math_teacher and is_jobless
   puts "You are a math teacher, but you have no job."
