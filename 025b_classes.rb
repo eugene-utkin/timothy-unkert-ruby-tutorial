f41eccf12250a0520f21b353529a709688d5551d
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-01-14 12:01:55 +0500

    Student class created

--- 025b_classes.rb
+++ 025b_classes.rb
@@ -15,3 +15,7 @@ puts "Your Ruby teacher is " + ruby_teacher.fname\
 + " " + ruby_teacher.lname + ", who is " + \
 ruby_teacher.age.to_s + " years old and whose website is " + \
 ruby_teacher.website.to_s + "."
+
+class Student < Teacher
+
+end
