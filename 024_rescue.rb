6e8c0719f67e5cb04b7b511d2c3a8662f486833f
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-01-30 10:03:16 +0500

    new line replace with space again

--- 024_rescue.rb
+++ 024_rescue.rb
@@ -3,7 +3,7 @@ my_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print "Enter two numbers to divide.\n"
 print "What is the numerator? "
 my_num = gets.chomp.to_f
-print "What is the denominator?\n"
+print "What is the denominator? "
 my_den = gets.chomp.to_f
 
 begin
