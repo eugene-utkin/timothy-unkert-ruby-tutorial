922832a54c57887b0a9cc6c55746d1e6bf8938a7
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-02-03 13:18:58 +0500

    comment added

--- 026_lambda_functions.rb
+++ 026_lambda_functions.rb
@@ -1,3 +1,5 @@
 # lambda functions are anonymous functions in ruby ... and python
 square_num = lambda { |num| num * num }
 cube_num = lambda { |num| num ** 3 }
+
+# we can pass a lambda function into another function
