9b3152ba4a25ce871bac4e6aea7c9e671eec179a
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-02-04 12:25:55 +0500

    number parameter added

--- 026_lambda_functions.rb
+++ 026_lambda_functions.rb
@@ -3,5 +3,5 @@ square_num = lambda { |num| num * num }
 cube_num = lambda { |num| num ** 3 }
 
 # we can pass a lambda function into another function
-def apply_lamby(lamby)
+def apply_lamby(lamby, number)
 end
