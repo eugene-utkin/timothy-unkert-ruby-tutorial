463acf3cedf132a5ca6c55d7e445582a00323afb
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-02-05 17:00:04 +0500

    another comment added

--- 027_procs.rb
+++ 027_procs.rb
@@ -1,2 +1,3 @@
 # A Proc object is an encapsulation of a block of code, which can be stored
 # in a local variable, passed to a method or another Proc, and can be called.
+# Might be called "closures" in other languages
